0b76c52b33a4e8f12daa00cc8b676cde4b219c6c
Craig Buchek <craig@boochtek.com>
2015-07-18 23:09:20 -0500

    Move DEFAULT_CONFIG_FILE to Provisional module

--- lib/provisional.rb
+++ lib/provisional.rb
@@ -6,5 +6,6 @@ require "provisional/image_list"
 module Provisional
 
   CONFIG_FILE = "config/infrastructure/provisional.yml"
+  DEFAULT_CONFIG_FILE = File.expand_path('../../data/default_config_file.yml', __FILE__)
 
 end
--- lib/provisional/init.rb
+++ lib/provisional/init.rb
@@ -3,10 +3,10 @@ require "fileutils"
 
 class Provisional::Init
 
-  attr_reader :global_options
+  attr_reader :options
 
-  def initialize(global_options)
-    @global_options = global_options
+  def initialize(options)
+    @options = options
   end
 
   def run
@@ -17,19 +17,15 @@ private
 
   def create_config_file
     FileUtils.mkdir_p(config_file_directory)
-    FileUtils.copy(default_config_file, config_file) unless File.exist?(config_file)
+    FileUtils.copy(Provisional::DEFAULT_CONFIG_FILE, config_file) unless File.exist?(config_file)
   end
 
   def config_file
-    global_options["config-file"]
+    options["config-file"]
   end
 
   def config_file_directory
     File.dirname(config_file)
-  end
-
-  def default_config_file
-    File.expand_path('../../../data/default_config_file.yml', __FILE__)
   end
 
 end
