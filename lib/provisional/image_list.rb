23378e589dc85f58a4f2229886a1fe828e730cb6
Craig Buchek <craig@boochtek.com>
2015-07-19 11:34:58 -0500

    List custom images too

--- lib/provisional/image_list.rb
+++ lib/provisional/image_list.rb
@@ -13,6 +13,9 @@ class Provisional::ImageList
     os_images.each do |image|
       display(image)
     end
+    custom_images.each do |image|
+      display(image)
+    end
   end
 
 private
@@ -29,12 +32,20 @@ private
     @os_images ||= all_images.select{|image| image.public}.select{|image| image.slug =~ /-\d/ || image.slug =~ /coreos/ }
   end
 
+  def custom_images
+    @custom_images ||= all_images.select{|image| !image.public && image.name =~ /-\d{14}$/}
+  end
+
   def all_images
     @all_images ||= digital_ocean.images.all.select{|image| image.type == "snapshot"}
   end
 
   def display(image)
-    puts "#{image.slug}: #{image.distribution} #{image.name}"
+    if image.public
+      puts "#{image.slug}: #{image.distribution} #{image.name}"
+    else
+      puts "#{image.name}"
+    end
   end
 
 end
