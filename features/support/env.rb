c8a4e16ef87e5ee0f6833cbce043f5a9886843a5
Craig Buchek <craig@boochtek.com>
2015-08-09 20:45:01 -0500

    Increase Aruba timeout, as our command takes a while to run
    Aruba's default timeout is 15 seconds. We're increasing that to 180 seconds for
    now. Note that the Aruba docs say to set `@aruba_timeout_seconds` in a `Before`
    block, but that didn't work. The `config.exit_timeout` does work.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,2 +1,8 @@
 require "bundler/setup"
 require "aruba/cucumber"
+
+
+# Our provisioning takes a while -- the 15-second default won't cut it.
+Aruba.configure do |config|
+  config.exit_timeout = 180
+end
